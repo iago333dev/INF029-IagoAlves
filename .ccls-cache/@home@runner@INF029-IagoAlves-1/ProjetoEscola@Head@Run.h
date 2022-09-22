f9915fb15bf3f2853c72e0fd7f0298792eeea00a
Iago_Alves333 <45635356+iago333dev@users.noreply.github.com>
2022-09-22 18:26:26 +0000

    remoção de lixo

--- .ccls-cache/@home@runner@INF029-IagoAlves-1/ProjetoEscola@Head@Run.h
+++
@@ -1,5 +0,0 @@
-int submenuStudantUpdate();
-int submenuTeacherUpdate();
-int showTeacherMenu();
-int showStudentMenu();
-int showSubjectsMenu();
--- .ccls-cache/@home@runner@INF029-IagoAlves-1/ProjetoEscola@Layout@Screen.c
+++
@@ -1,9 +0,0 @@
-int ExitFunction(){
-  int any;
-  
-  printf("Pressione 0 Para continuar... \n");
-  scanf("%d",&any);
-  system("clear");
-
-  return 0;
-}
